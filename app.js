d5c5500260b2d441996aab6bcfb765ebd896632b
naylaqanitaalifia <naylaqanita286@gmail.com>
2026-07-01 08:42:27 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Employee Management API Running",
+    message: "Employee Management API Running.",
   });
 });
 
