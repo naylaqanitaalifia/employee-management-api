de5c6df3c847731e84f0ded924ab85921e826523
naylaqanitaalifia <naylaqanita286@gmail.com>
2026-08-11 10:37:56 +0700

    fix

--- src/routes/leave.route.js
+++ src/routes/leave.route.js
@@ -15,7 +15,7 @@ router.get("/", getAllLeaves);
 router.get("/:id", getLeaveById);
 router.post("/", createLeave);
 router.put("/:id", updateLeave);
-router.delete("/:id", deleteLeave);
+// router.delete("/:id", deleteLeave);
 router.patch("/:id/reject", rejectLeave);
 router.patch("/:id/approve", approveLeave);
 
